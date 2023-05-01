31ef4bc8f6ddd078ada0790f21e17131a5c4873e
Sv-L <121663409+Sv-L@users.noreply.github.com>
2023-05-01 17:50:12 +0000

    Deploying to gh-pages from @ Sv-L/goit-js-hw-08@8937229d36fb12ccff63c326b51232b81866752a 🚀

---
+++ 02-video.2ea735ae.js
@@ -0,0 +1,2 @@
+document.querySelector("#vimeo-player").addEventListener("touchmove",handleTouchMove,{passive:!0});
+//# sourceMappingURL=02-video.2ea735ae.js.map
---
+++ 02-video.ec464cb4.js
@@ -0,0 +1,2 @@
+document.querySelector("#vimeo-player").addEventListener("touchmove",handleTouchMove,{passive:!0});
+//# sourceMappingURL=02-video.ec464cb4.js.map
