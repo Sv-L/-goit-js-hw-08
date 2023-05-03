2d466fb82b36dfe3ed57cd3b15e06311b7d0a56c
liulka_s <asl.warmhaus@gmail.com>
2023-05-04 01:41:18 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const iframePlayer = new Player(iframe);
+let currentTime;
+const trottleTimeupdatePlayer = throttle(onTimeupdate, 1000);
+iframePlayer.on('timeupdate', trottleTimeupdatePlayer);
+function onTimeupdate(e) {
+  currentTime = e.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  iframePlayer.setCurrentTime(savedTime);
+}
