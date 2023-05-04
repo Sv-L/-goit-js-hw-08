63b7b889744335f6973853cf89ee5615ac3ddde8
liulka_s <asl.warmhaus@gmail.com>
2023-05-04 18:26:10 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,18 @@
 import throttle from 'lodash.throttle';
-
 const formEl = document.querySelector('.feedback-form');
-const throttleOnInput = throttle(onInput, 500);
+const emailInput = formEl.querySelector('input[name="email"]');
+const messageInput = formEl.querySelector('textarea[name="message"]');
+
 const STORAGE_KEY = 'feedback-form-state';
-let formState = {};
+const throttleOnInput = throttle(onInput, 500);
 
 formEl.addEventListener('input', throttleOnInput);
 formEl.addEventListener('submit', onSubmit);
-
 function onInput(e) {
-  formState[e.target.name] = e.target.value;
+  let formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
 }
 
@@ -23,7 +26,6 @@ if (localStorage.getItem(STORAGE_KEY)) {
     console.log(error.message);
   }
 }
-
 function onSubmit(e) {
   e.preventDefault();
   if (formEl.email.value === '') {
@@ -32,6 +34,5 @@ function onSubmit(e) {
     console.log(localStorage.getItem(STORAGE_KEY));
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
-    formState = {};
   }
 }
