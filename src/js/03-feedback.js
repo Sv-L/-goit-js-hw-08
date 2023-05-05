698d3c16f031229759610751a72de1e0fa5082da
liulka_s <asl.warmhaus@gmail.com>
2023-05-05 05:52:18 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,29 +8,31 @@ let formState = {};
 formEl.addEventListener('input', throttleOnInput);
 formEl.addEventListener('submit', onSubmit);
 
-function onInput(e) {
-  formState[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
-}
-
 if (localStorage.getItem(STORAGE_KEY)) {
   try {
     formState = JSON.parse(localStorage.getItem(STORAGE_KEY));
   } catch (error) {
     console.log(error);
   }
-  const formStateKeys = Object.keys(formState);
+  fillFormFields(formState, formEl);
+}
 
-  for (let i = 0; i < formEl.elements.length; i++) {
-    const element = formEl.elements[i];
-
-    if (element.tagName === 'input' || element.tagName === 'textarea') {
+function fillFormFields(storageValue, form) {
+  const formStateKeys = Object.keys(storageValue);
+  for (let i = 0; i < form.elements.length; i++) {
+    const element = form.elements[i];
+    if (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA') {
       const fieldName = element.name;
       if (formStateKeys.includes(fieldName)) {
-        element.value = formState[fieldName];
+        element.value = storageValue[fieldName];
       }
     }
   }
+}
+
+function onInput(e) {
+  formState[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
 }
 
 function onSubmit(e) {
